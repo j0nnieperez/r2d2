3e45387ea3ec090f57c8f08d3fb091d02a95d79a
Jonathan Perez <jonathan.e.perezjimenez@gmail.com>
2019-03-21 13:54:00 -0600

    solved ToReturn Funcitons

--- r2d2.ino
+++ r2d2.ino
@@ -1,5 +1,5 @@
-int M1_1 = 2;
-int M1_2 = 3; 
+int M1_1 = 2; // Pin 2 conectado al motor 1
+int M1_2 = 3; // Pin 3 conectado al motor 1
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,7 +9,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  ToReturn();
+  delay(500);
+  Stop();
+  delay(500);
+  Advance();
+  delay(500);
 }
 
 void Advance(){
@@ -17,6 +22,12 @@ void Advance(){
   digitalWrite(M1_2, LOW);
 }
 
+void Stop(){
+  digitalWrite(M1_1, LOW);
+  digitalWrite(M1_2, LOW);
+}
+
 void ToReturn(){
-  
+  digitalWrite(M1_1, LOW);
+  digitalWrite(M1_2, HIGH);
 }
