136a4b043eac32025c3fb190bde271c81b9261d8
Jonathan Perez <jonathan.e.perezjimenez@gmail.com>
2019-03-21 13:19:25 -0600

    principal functions

--- r2d2.ino
+++ r2d2.ino
@@ -1,9 +1,22 @@
+int M1_1 = 2;
+int M1_2 = 3; 
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(M1_1, OUTPUT);
+  pinMode(M1_2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+}
 
+void Advance(){
+  digitalWrite(M1_1, HIGH);
+  digitalWrite(M1_2, LOW);
+}
+
+void ToReturn(){
+  
 }
