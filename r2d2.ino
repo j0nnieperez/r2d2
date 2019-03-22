28f2560a404e38d130a96a7d238117817728e185
Milton Ramos <jyehovani@gmail.com>
2019-03-22 13:41:01 -0600

    agrego rotacion

--- r2d2.ino
+++ r2d2.ino
@@ -1,10 +1,15 @@
 int M1_1 = 2;
 int M1_2 = 3; 
+int turnR = 4; //giro a derecha 
+int turnL = 5; //giro a izquierda
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(M1_1, OUTPUT);
   pinMode(M1_2, OUTPUT);
+  pinMode(turnL, OUTPUT);
+  pinMode(turnR, OUTPUT);
 }
 
 void loop() {
@@ -16,6 +21,16 @@ void Advance(){
   digitalWrite(M1_1, HIGH);
   digitalWrite(M1_2, LOW);
 }
+
+void GiroI(){
+  digitalWrite(turnL, HIGH);
+  digitalWrite(turnR, LOW); 
+}
+void GiroI(){
+  digitalWrite(turnR, HIGH);
+  digitalWrite(turnL, LOW); 
+}
+
 
 void ToReturn(){
   
